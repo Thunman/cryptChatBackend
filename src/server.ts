1edd8a28447a8289ca88408b502c74b6b6efb597
Thunman <Thunman42@gmail.com>
2024-03-02 13:01:47 +0100

    adding db connection

--- src/server.ts
+++ src/server.ts
@@ -3,26 +3,31 @@ import dotenv from "dotenv";
 import https from "https";
 import cors from "cors";
 import { readFileSync } from "fs";
+import { userRouter } from "./routers/userRouter";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
+const mongoUrl = process.env.MONGO_URL || "";
 const port = process.env.PORT || 3001;
-const sslKey: string = process.env.SSL_KEY || "";
-const sslCert: string = process.env.SSL_CERT || "";
+const sslKey = process.env.SSL_KEY || "";
+const sslCert = process.env.SSL_CERT || "";
 const options = {
 	key: readFileSync(sslKey),
 	cert: readFileSync(sslCert),
 };
 app.use(express.json());
+app.use("/api/users", userRouter);
 app.use(cors({ origin: "http://localhost:3000" }));
 app.set("trust proxy", 1);
 
 const server = https.createServer(options, app);
 
-const startServer = () => {
+const startServer = async () => {
 	try {
 		server.listen(port);
+		await mongoose.connect(mongoUrl);
 	} catch (error) {
 		console.error("Error staring server: ", error);
 	}
