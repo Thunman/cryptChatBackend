8b7bd499a402a594296ba680e6477d24cbb0bce8
Thunman <Thunman42@gmail.com>
2024-03-02 16:22:41 +0100

    finnishing login and register userController, adding jwt for authentication

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,8 +1,9 @@
 import { validationResult } from "express-validator";
-import { registerValidators } from "../middleware/validators";
+import { loginValidators, registerValidators } from "../middleware/validators";
 import { Request, Response } from "express";
 import { User } from "../models/models";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userController = {
 	register: [
@@ -24,8 +25,40 @@ const userController = {
 					email,
 					password: hashedPassword,
 				});
+				await newUser.save();
+				res.status(201).json({ message: "User Created" });
 			} catch (error) {
 				console.error(error);
+				res.status(500).json({ message: "Server Error" });
+			}
+		},
+	],
+	login: [
+		...loginValidators,
+		async (req: Request, res: Response) => {
+			try {
+				const { email, password } = req.body;
+				const user = await User.findOne({ email });
+				if (!user || (await bcrypt.compare(password, user.password)))
+					return res
+						.status(401)
+						.json({ message: "Invalid Email or Password" });
+				const token = jwt.sign(
+					{
+						id: user._id,
+						email: user.email,
+					},
+					process.env.JWT_SECRET
+				);
+				res.cookie("token", token, {
+					httpOnly: true,
+					secure: true,
+					sameSite: "none",
+				});
+				res.status(200);
+			} catch (error) {
+				console.error(error);
+				res.status(500).json({ message: "Server Error" });
 			}
 		},
 	],
